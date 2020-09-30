945ba68b773e6a7225dbce8de76f290a12a24167
Vinay Bhide <60889328+vinaybhide@users.noreply.github.com>
2020-09-30 23:56:50 +0530

    MF Support V0.1

---
+++ mselectportfolioMF.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Select MF Portfolio" Language="C#" MasterPageFile="~/Site.Mobile.Master" AutoEventWireup="true" CodeBehind="mselectportfolioMF.aspx.cs" Inherits="Analytics.mselectportfolioMF" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h3 style="text-align:center; margin-top:2%;">Select Portfolio</h3>
+    <div style="text-align:center; padding: 10% 5% 10% 5%; border: solid"">
+        <asp:Label ID="label3" Text="Select portfolio to open:" runat="server"></asp:Label><br />
+        <asp:DropDownList ID="ddlPortfolios" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlPortfolios_SelectedIndexChanged"></asp:DropDownList>
+        <br />
+        <br />
+        <asp:Label ID="labelSelectedFile" runat="server" Text="Selected File: Please select portfolio to open" ></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="buttonLoad" runat="server" Text="Open Portfolio" OnClick="buttonLoad_Click" />
+    </div>
+</asp:Content>
---
+++ mselectportfolioMF.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Analytics
+{
+    public partial class mselectportfolioMF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if ((Session["EmailId"] != null) || (Session["PortfolioFolderMF"] != null))
+            {
+                if (!IsPostBack)
+                {
+                    string folder = Session["PortfolioFolderMF"].ToString();
+                    string[] filelist = Directory.GetFiles(folder, "*.mfl");
+                    Session["MFName"] = null;
+                    //int lstwidth = 0;
+
+                    ListItem li = new ListItem("Select MF Portfolio", "-1");
+                    ddlPortfolios.Items.Insert(0, li);
+
+                    foreach (string filename in filelist)
+                    {
+                        string portfolioName = filename.Remove(0, filename.LastIndexOf('\\') + 1);
+                        ListItem filenameItem = new ListItem(portfolioName, filename);
+                        ddlPortfolios.Items.Add(filenameItem);
+                    }
+                    //listboxFiles.Width = lstwidth * 10;
+                    bool isValuation = false;
+                    if (Request.QueryString["valuation"] != null)
+                        isValuation = System.Convert.ToBoolean(Request.QueryString["valuation"]);
+
+                    if (isValuation)
+                    {
+                        buttonLoad.Text = "Show MF Portfolio Valuation";
+                    }
+                    else
+                    {
+                        buttonLoad.Text = "Open MF Portfolio";
+                    }
+                }
+            }
+            else
+            {
+                //Response.Redirect(".\\Default.aspx");
+                //Response.Write("<script language=javascript>alert('" + common.noLogin + "')</script>");
+                Page.ClientScript.RegisterStartupScript(GetType(), "myScript", "alert('" + common.noLogin + "');", true);
+                Response.Redirect("~/Default.aspx");
+            }
+        }
+
+        protected void buttonLoad_Click(object sender, EventArgs e)
+        {
+            //string selectedFile = listboxFiles.SelectedValue;
+            if (ddlPortfolios.SelectedIndex > 0)
+            {
+                Session["PortfolioNameMF"] = ddlPortfolios.SelectedValue;
+                Session["ShortPortfolioNameMF"] = ddlPortfolios.SelectedItem.Text;
+                bool isValuation = false;
+                if (Request.QueryString["valuation"] != null)
+                    isValuation = System.Convert.ToBoolean(Request.QueryString["valuation"]);
+
+                if (isValuation == false)
+                {
+                    //Server.Transfer("~/openportfolio.aspx");
+                    //if (this.MasterPageFile.Contains("Site.Master"))
+                    //    Response.Redirect("~/openportfolioMF.aspx");
+                    //else if (this.MasterPageFile.Contains("Site.Mobile.Master"))
+                    //    Response.Redirect("~/mopenportfolioMF.aspx");
+                    //else
+                        Response.Redirect("~/mopenportfolioMF.aspx");
+                }
+                else
+                {
+                    string url = "~/portfoliovaluationMF.aspx" + "?";
+
+                    //if (this.MasterPageFile.Contains("Site.Master"))
+                    //{
+                    //    url += "parent=openportfolioMF.aspx";
+                    //    ResponseHelper.Redirect(Response, url, "_blank", "menubar=0,scrollbars=2,width=1280,height=1024,top=0, left=0");
+                    //}
+                    //else if (this.MasterPageFile.Contains("Site.Mobile.Master"))
+                    //{
+                    //    url += "parent=mopenportfolioMF.aspx";
+                    //    ResponseHelper.Redirect(Response, url, "_blank", "menubar=0,scrollbars=2,width=1280,height=1024,top=0, left=0");
+                    //}
+                    url += "parent=mopenportfolioMF.aspx";
+                    ResponseHelper.Redirect(Response, url, "_blank", "menubar=0,scrollbars=2,width=1280,height=1024,top=0, left=0");
+                }
+            }
+            else
+            {
+                labelSelectedFile.Text = "Selected Portfolio: Please select valid portfolio to open";
+                //Response.Write("<script language=javascript>alert('" + common.noPortfolioNameToOpen +"')</script>");
+                Page.ClientScript.RegisterStartupScript(GetType(), "myScript", "alert('" + common.noPortfolioNameToOpen + "');", true);
+            }
+        }
+        protected void ddlPortfolios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (ddlPortfolios.SelectedValue.Equals("-1") == true)
+            {
+                labelSelectedFile.Text = "Selected Portfolio: Please select valid portfolio to open";
+            }
+            else
+            {
+                labelSelectedFile.Text = "Selected Portfolio: " + ddlPortfolios.SelectedItem.Text;
+            }
+        }
+
+    }
+}
